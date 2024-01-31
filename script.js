952d21c130662cd38d6d76627b34e83f7e1fe607
Ilgor Albertino <ilgoralbertino18@gmail.com>
2024-01-31 10:03:29 -0300

    Convert-Money2.0

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
 const convertButton = document.querySelector(".convert-button");
 const currencySelect = document.querySelector(".currency-select");
 
-function convertValues() {
+
+
+const convertValues = async () => {
   const inputCurrencyValue = document.querySelector(".input-currency").value;
   const currencyValueToConvert = document.querySelector(".currency-value-to-convert"); // Valor para converter
   const currencyValueConverted = document.querySelector(".currency-value"); // Valor Convertido em outra moeda
 
-  const dolarToday = 5.2;
-  const euroToday = 6.2;
+  const data = await fetch(" https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL").then( response => response.json())
+  
+  const dolarToday = data.USDBRL.high
+  const euroToday = data.EURBRL.high
   const bitcoinToday = 173647.09;
 
-  if (currencySelect.value == "dolar"  ) {
+   
+
+   if (currencySelect.value == "dolar"  ) {
     currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
       style: "currency",
       currency: "USD",
